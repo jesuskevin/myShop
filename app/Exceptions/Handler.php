c7f1a69c4921206b0e7c249ade1044f177246a47
Kevin Gomez <de.jesuskevin01@hotmail.com>
2023-06-29 16:31:00 -0400

    Make login page the default page & global exception handler configuration

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -25,6 +26,49 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (Throwable $e, $request) {
+            if ($e instanceof \Illuminate\Session\TokenMismatchException) {
+                info($e);
+                return redirect('/login');
+            } elseif ($e instanceof \Symfony\Component\HttpKernel\Exception\HttpException) {
+                if ($e->getStatusCode() == 419) {
+                    info($e);
+                    return redirect('/login')->with('error', 'Your session expired due to inactivity. Please login again.');
+                } elseif ($e->getStatusCode() == 403) { //The only way to prevent Laravel from displaying 403 page
+                    info($e);
+                    return redirect('/')->with('error', 'We could not find the page you were trying to access.');
+                } elseif ($e->getStatusCode() == 404) {
+                    info($e);
+                    return redirect('/')->with('error', 'We could not find the page you were trying to access.');
+                } elseif ($e->getStatusCode() == 405) { //Invalid http method for route
+                    info($e);
+                    return redirect('/')->with('error', 'We could not find the page you were trying to access.');
+                }
+            } elseif ($e instanceof \Illuminate\Validation\ValidationException) {
+                if ($request->expectsJson()) {
+                    info($e);
+                    return response()->json($e->validator->errors()->all(), 422);
+                } else {
+                    info($e);
+                    return back()->withErrors($e->validator)->withInput();
+                }
+            } elseif ($e instanceof AuthenticationException) {
+                if ($request->route()->uri() == '/') {
+                    info($e);
+                    return redirect('/login');
+                }
+            } elseif ($e instanceof \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException) {
+                info($e);
+                return back()->with('error', 'We could not find the page you were trying to access.');
+            } elseif ($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+                info($e);
+                return back()->with('error', 'We could not find the page you were trying to access.');
+            } else {
+                info($e);
+                return back()->with('error', 'An error occured, please try again or contact support if the problem persists.')->withInput();
+            }
         });
     }
 }
